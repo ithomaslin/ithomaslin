579880eb810a6a6de25ddbb2adb41e2c5da2c3d1
ithomaslin <ithomas.lin@me.com>
2013-12-16 23:35:39 +0000

    Add user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Ithomaslin::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://ithomaslin.me' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ithomaslin::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "blog" => "pages#blog"
 
