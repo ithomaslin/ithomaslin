2876dd6b4ef4caebfa675ba739451d3dec1e4b90
ithomaslin <ithomas.lin@me.com>
2013-12-16 00:32:56 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Ithomaslin::Application.routes.draw do
+  root "pages#home"
+  get "blog" => "pages#blog"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
